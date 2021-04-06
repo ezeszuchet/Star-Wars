090dde8103bcd1296580fea2dbdfc4ba3913146b
ezeszuchet <szuchetezequiel@gmail.com>
2021-04-06 20:57:57 -0300

    Add Navbar to fix

--- star-wars-front/src/App.js
+++ star-wars-front/src/App.js
@@ -1,8 +1,12 @@
 import { FooterContainer } from './containers/footer'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <>
+      <Navbar />
+
+      
       <FooterContainer />
     </>
   );
